67d93fd72ac3379aeaf9a7a56dd3f587c0f6d93e
Kim Batselier <kim.batselier@gmail.com>
2013-11-11 10:19:26 +0800

    getLCM.m now also has an SVD-based implementation.

--- getLCM.m
+++ getLCM.m
@@ -1,34 +1,41 @@
-function [lcm h e] = getLCM(fsys,gsys,noiselevel)
-% [lcm h e] = getLCM(fsys,gsys,tol)
-% ---------------------------------
+function [lcm h e] = getLCM(fsys,gsys,noiselevel,notsparse)
+% [lcm h e] = getLCM(fsys,gsys,noiselevel,notsparse)
+% --------------------------------------------------
 % Returns the approximate least common multiple of two given polynomials fsys and gsys. Also
 % the factor h such that lcm = g*h is calculated.
 %
-% lcm       =   vector, coefficient vector of polynomial which is the least
-%               common multiple of f and g
+% lcm       	=   vector, coefficient vector of polynomial which is the least
+%               	common multiple of f and g
 %
-% h         =   vector, coefficient vector of polynomial which is the 
-%               factor such that lcm = g*h
+% h         	=   vector, coefficient vector of polynomial which is the 
+%               	factor such that lcm = g*h
 %
-% e         =   scalar, mean squared error of least squares solution
-%               lcm = g * h
+% e         	=   scalar, mean squared error of least squares solution
+%               	lcm = g * h
 %
-% fsys      =   cell, polysys representation of the polynomial f
+% fsys      	=   cell, polysys representation of the polynomial f
 %
-% gsys      =   cell, polysys representation of the polynomial g
+% gsys      	=   cell, polysys representation of the polynomial g
 %
-% tol       =   scalar, tolerance: measure of how many digits are not
-%               corrputed by noise. Default: eps
+% noiselevel	=   scalar, upper bound on the error on the coefficients
+%					of fsys and gsys. Default: tolerance for numerical rank Macaulay matrix
+%
+% notsparse     =   boolean, if set to 0, then a sparse rank revealing QR
+%                   is used. If set to 1, a dense SVD is used instead.
+%                   Default is 0.
 %
 % CALLS
 % -----
 % 
-% getM.m, getD0.m, getMex.m
+% getM.m, getD0.m, getMex.m, updateOrth.m, updateN.m
 %
 % Kim Batselier, 2011-01-23, edited 2012-01-31: accepts polysys objects now
 % warning off all
 if nargin == 2
     noiselevel = 0;
+    notsparse = 0;
+elseif nargin == 3
+	notsparse = 0;
 end    
 
 % check whether both f and g have same number of variables
@@ -54,16 +61,29 @@ e = 0;
 % need to start iteration over max(deg(f),deg(g))
 d = max(df,dg);
 
-% initialize the Macaulay matrices
-Mf = getM(fsys,d,1);
-[Q , ~, P] = qr(Mf','vector'); % very painful if c(d) is comparable to q(d)
-Qf=Q(:,size(Mf,1));   % always of full row rank
-Nf=Q(:,size(Mf,1)+1:end);
+% initialization Macaulay matrix and orthogonal basis kernel
+if ~notsparse
+    Mf = getM(fsys,d,1);
+    [Q , R, P] = qr(Mf','vector'); % very painful if c(d) is comparable to q(d)
+    Qf=Q(:,size(Mf,1));   % always of full row rank
+	Nf=Q(:,size(Mf,1)+1:end);
 
-Mg = getM(gsys,d,1);
-[Q , ~, P] = qr(Mg','vector'); % very painful if c(d) is comparable to q(d)
-Ng = Q(:,size(Mg,1)+1:end); 
-tol = 20*sum(size(Mf))*eps;
+	Mg = getM(gsys,d,1);
+	[Q ,R, P] = qr(Mg','vector'); % very painful if c(d) is comparable to q(d)
+	Ng = Q(:,size(Mg,1)+1:end); 
+	tol = 20*sum(size(Mf))*eps;
+else
+    Mf=getM(fsys,d)';
+    [U S V]=svd(Mf);
+    s=diag(S);
+    tol=max(size(Mf))*eps(s(1));
+    r=sum(s > tol );
+    Qf=U(:,1:r);
+    Nf=U(:,r+1:end);
+    
+    Mg = getM(gsys,d);
+    Ng=null(Mg);
+end
 
 
 while isempty(lcm) && d <= df+dg
@@ -71,7 +91,7 @@ while isempty(lcm) && d <= df+dg
     if noiselevel % only for nonzero noiselevel we need to overwrite the tolerance
         tol=sqrt(2)*( cond(getM(fsys,d)) + cond(getM(gsys,d)) )*noiselevel;
     end
-    [~, S Z] = svd(full(Ng'*Qf));
+    [Y, S Z] = svd(full(Ng'*Qf));
     
     if size(S,2)==1
         s=S(1);
@@ -84,14 +104,20 @@ while isempty(lcm) && d <= df+dg
         lcm = sparse(Qf*Z(:,end))';
         % decomposition of lcm in M via qr
         % Mg is always of full row rank!
-        
+        [asin(s) d tol]
         M=getM(gsys,d,1);        
         h =  (M'\lcm')';   
         e = norm(lcm-h*M);
     else
       d=d+1;
-      [Qf Nf tol] = updateOrth(Qf,Nf,getMex(fsys,d,d-1,1),1);
-      Ng = updateN(Ng,getMex(gsys,d,d-1,1),1);      
+      if ~notsparse
+		[Qf Nf tol] = updateOrth(Qf,Nf,getMex(fsys,d,d-1,1),1);
+		Ng = updateN(Ng,getMex(gsys,d,d-1,1),1);        
+	  else
+		[Qf Nf tol] = updateOrth(Qf,Nf,getMex(fsys,d,d-1,1));
+		Ng = updateN(Ng,getMex(gsys,d,d-1,1));      
+	  end
+      
     end
 end
     
