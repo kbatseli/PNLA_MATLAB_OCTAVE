78f0d48358746a243b60fb3699b68b3c270e4d6b
Kim Batselier <kim.batselier@gmail.com>
2013-12-04 21:49:07 +0800

    sparse implementation without matrix multiplications

--- sparseN.m
+++ sparseN.m
@@ -1,63 +1,116 @@
-function [N nM tol]=sparseN(polysys,d)
+function [N, nM, tol]=sparseN(polysys,d)
+% [N, nM, tol]=sparseN(polysys,d)
+% -------------------------------
+% Computes a sparse basis for the null space of the Macaulay matrix for a
+% given polynomial system polysys and degree d.
+%
+% N             =   matrix, sparse basis for null space of M(d)
+%
+% nM            =   vector, 
+%
+% tol           =   scalar, the tolerance used for the rank-test,
+%                   max(size(M(d))) * eps(s), where s is an upper bound on
+%                   the largest singular value of M(d)
+%
+% polysys       =   cell containing coefficients and monomials exponents of the
+%                   set of polynomial equations.
+%
+% d             =	scalar, degree for which M(d) has to be constructed from polysys
+%
+% CALLS
+% -----
+%
+% getM.m
+%
+% Kim Batselier, december 2013
 
 s=size(polysys,1);
 n=size(polysys{1,2},2);
 
+% normalize each coefficient vector to unit 1-norm such that the largest
+% singular value of M(d) is upper bounded by sqrt(s)
 for i=1:s
     polysys{i,1}=polysys{i,1}/norm(polysys{i,1},1);
 end
 
 % if M is sparse column compressed than accessing columns
 % is faster than rows
-M=getM(polysys,d,1); 
-[p q]=size(M);
+initialM=getM(polysys,d,1); 
+[p, q]=size(initialM);
 
 tol=max(p,q)*eps(sqrt(s));
 nM=zeros(1,p);
-nM(1)=norm(M(1,:));
+nM(1)=norm(initialM(1,:));
 
-nindex=find(M(1,:));
+% indices of nonzero coefficients
+nindex=find(initialM(1,:));
+% indices of zero coefficients
 zindex=setdiff(1:q,nindex);
 
-% initial way of constructing dense N
-% N=zeros(q,q-1);
-% for i=1:length(zindex)
-%     N(zindex(i),i)=1;
-% end
+%% initalize sparse N, first time we need to make it explicit
+% column indices
+colI=zeros(1,length(zindex)+2*(length(nindex)-1));
+colI(1:length(zindex))=1:length(zindex); % need to finish remaining indices
 
-% initalize sparse N, first time we need to make it explicit
-N=sparse(zindex,1:length(zindex),ones(1,length(zindex)),q,q-1);
+% row indices
+rowI=zeros(1,length(zindex)+2*(length(nindex)-1));
+rowI(1:length(zindex))=zindex; % need to finish the remaining indices
 
-pivotI=nindex(find(abs(M(1,nindex))==max(abs(M(1,nindex))),1));
-M(1,:)=M(1,:)/M(1,pivotI);
+% values
+values=zeros(1,length(zindex)+2*(length(nindex)-1));
+values(1:length(zindex))=ones(1,length(zindex));
+
+pivotI=nindex(find(abs(initialM(1,nindex))==max(abs(initialM(1,nindex))),1));
+initialM(1,:)=initialM(1,:)/initialM(1,pivotI);
 tempI=setdiff(nindex,pivotI);
 
-for j=1:length(tempI)
-    N(pivotI,length(zindex)+j)=M(1,tempI(j));
-    N(tempI(j),length(zindex)+j)=-M(1,pivotI);       
-end
-M=M*N;
-[p q]=size(M);
+% make the new M
+M=spalloc(p-1,q-1,nnz(initialM));
+M(:,1:length(zindex))=initialM(2:end,zindex);
 
-for i=2:p
-    nM(i)=norm(M(i,:));
-    if nM(i) > tol
-        nindex=find(M(i,:));
+% finish M and remaining indices/values for initialN
+for j=1:length(tempI)
+    % remaining columns of M
+    M(:,length(zindex)+j)=initialM(1,tempI(j))*initialM(2:end,pivotI)-initialM(1,pivotI)*initialM(2:end,tempI(j));
+    
+    % remaining indices that determine new N
+    colI(length(zindex)+2*(j-1)+1:length(zindex)+2*j)=(length(zindex)+j)*ones(1,2);
+    rowI(length(zindex)+2*(j-1)+1:length(zindex)+2*j)=[pivotI tempI(j)];
+    values(length(zindex)+2*(j-1)+1:length(zindex)+2*j)=[initialM(1,tempI(j)) -initialM(1,pivotI)];    
+end
+
+% now we can make the first N
+N=sparse(rowI,colI,values,q,q-1,length(zindex)+2*(length(nindex)-1));
+[p,q]=size(M);
+
+for i=1:p
+    nM(i+1)=norm(M(1,:));
+    if nM(i+1) > tol
+        nindex=find(M(1,:));
         zindex=setdiff(1:q,nindex);
-        tempN=zeros(q,q-1);
-        for j=1:length(zindex)
-            tempN(zindex(j),j)=1;
-        end
-        %     temp=full(sparse(zindex,1:length(zindex),ones(1,length(zindex)),q,length(zindex)));
-        pivotI=nindex(find(abs(M(i,nindex))==max(abs(M(i,nindex))),1));
+        
+        pivotI=nindex(find(abs(M(1,nindex))==max(abs(M(1,nindex))),1));
         tempI=setdiff(nindex,pivotI);
-        M(i,:)=M(i,:)/M(i,pivotI);
+        M(1,:)=M(1,:)/M(1,pivotI);
+          
+        tempM=M;
+        M=spalloc(p-1,q-1,nnz(tempM));
+        M(:,1:length(zindex))=tempM(2:end,zindex);
+        
+        tempN=N;
+        N=spalloc(size(N,1),q-1,nnz(tempN));
+        N(:,1:length(zindex))=tempN(:,zindex);
+        
         for j=1:length(tempI)
-            tempN(pivotI,length(zindex)+j)=M(i,tempI(j));
-            tempN(tempI(j),length(zindex)+j)=-M(i,pivotI);       
+            % remaining columns of M            
+            M(:,length(zindex)+j)=tempM(1,tempI(j))*tempM(2:end,pivotI)-tempM(1,pivotI)*tempM(2:end,tempI(j));
+            
+            N(:,length(zindex)+j)=tempM(1,tempI(j))*tempN(:,pivotI)-tempM(1,pivotI)*tempN(:,tempI(j));    
         end
-        M=M*tempN;
-        [p q]=size(M);
-        N=N*tempN;    
+        [p, q]=size(M);
+    else
+       % first row of M is numerically zero, remove it
+       M=M(2:end,:);
+       [p,q]=size(M); 
     end
 end
