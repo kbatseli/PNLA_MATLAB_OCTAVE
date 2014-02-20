28dec09f21df72ec4ad4e6cef2e79673361eea68
Kim Batselier <kim.batselier@gmail.com>
2014-02-20 17:38:45 +0800

    fixed getNS.m bug, thank you Antoine Vandermeersch :)

--- getNS.m
+++ getNS.m
@@ -2,9 +2,12 @@ function [NS normalset] = getNS(polysys,d)
 % [NS normalset] = getNS(polysys,d)
 % -----------------------------------
 %
-% Calculates the standard monomials (Normal Set) which are also a basis for
-% the quotient space R/I where R refers to the Polynomial Ring and I to an
-% Ideal.
+% Obsolete: It is better (more robust) to compute the normal set monomials via
+% the canonical decomposition method candecomp.m rather than with this method.
+% Calculates the standard monomials (Normal Set) for a given degree d 
+% of the Macaulay matrix. These monomials are also a basis for
+% the quotient space R/I where R refers to the Polynomial Ring and I to a
+% polynomial ideal for a large enough degree d.
 %
 % NS        =   matrix, each row contains the exponents of a standard
 %               monomial
@@ -20,24 +23,23 @@ function [NS normalset] = getNS(polysys,d)
 % CALLS
 % -----
 %
-% getM.m,  
+% getM.m,  spqr.m (SuiteSparse package)
 %
-% Kim Batselier, 2010-11-04, update: now uses sparseqr and permutations
-% as in sparf.m and polyDiv.m
+% Kim Batselier
 
 n = size(polysys{1,2},2);
 
 M = getM(polysys,d,1);   
 
-% %% determine rank of Divisor matrix
-[Q R P] = qr(M',struct('Q','matrix','permutation','vector'));
+% %% determine rank of Macaulay matrix
+[Q R P] = spqr(M',struct('Q','matrix','permutation','vector'));
 rankM = length(find(diag(R)));
 V = Q(:,rankM+1:end);
 
 clear Q R P
 
 % Determine normal set that will span the remainder
-[Qv Rv Pv] = qr(V',struct('Q','discard','permutation','vector'));
+[Qv Rv Pv] = spqr(V',struct('Q','discard','permutation','vector'));
 normalset = sort(Pv(1:size(V,2)));
 NS = zeros(size(V,2),n);
 for i = 1 : size(V,2)
